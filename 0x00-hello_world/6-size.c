93c6fdbb8159b1de2f4da17ffdd051ce462f1149
zicodinger <zicodinger@gmail.com>
2022-12-12 14:04:22 -0800

    Intel

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - Entry Point
+ *
+ * Return: Always 0 (Successful)
+ */
+
+int main(void)
+{
+	printf("size of a char: %lu byte(s)\n", sizeof(char));
+	printf("size of an int: %lu byte(s)\n", size of(int));
+	printf("size of a long int: %lu byte(s)\n", size of(long int)),
+	printf("size of a long long int: %lu bytes(s)\n", size of(long long int)),
+	printf("size of a float: %lu byte(s)\n", size of(float)),
+	return (0);
+
+}
