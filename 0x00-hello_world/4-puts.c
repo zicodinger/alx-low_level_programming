16408c07547fa95ffbe8c1b7f63bc18c5e9da730
zicodinger <zicodinger@gmail.com>
2022-12-12 06:39:16 -0800

    Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,14 @@
-#iclude <stdio.h>
+#include <stdio.h>
+
 /**
- * main -  Entry
- * Return: Always 0
+ * main - Entry Point
+ *
+ * Return: Always (0) (Success)
  */
+
 int main(void)
 {
-	puts("\"Programming is like building a multi lingual puzzle);
-	retturn (0);
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+
 }
